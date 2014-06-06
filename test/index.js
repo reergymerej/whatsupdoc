01229f6705ac13c6d2b6d03905f8135a81033c37
Jeremy Greer <jeremygreer@reergymerej.com>
2014-06-05 21:31:34 -0400

    get tests set up

--- test/index.js
+++ test/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+var mocha = require('mocha'),
+    will = require('willy').will,
+    slutDoc = require('../index');
+
+describe('getting started', function () {
+    it('should work', function () {
+        will(function () {
+            slutDoc.index();
+        }).not.throw();
+    });
+});
