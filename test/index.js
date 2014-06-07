b75abf1fb76afddec7fb2d0501bec4397106cda9
Jeremy Greer <jeremygreer@reergymerej.com>
2014-06-06 22:29:22 -0400

    start adding regex

--- test/index.js
+++ test/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var mocha = require('mocha'),
+/*global beforeEach */
+
+var path = require('path'),
+    mocha = require('mocha'),
     willy = require('willy'),
     will = willy.will,
     slutDoc = require('../slut-doc');
 
-var path = process.cwd();
+var searchPath = path.join(__dirname, 'dummy-source');
 
 willy.addTest(function beGreaterThan(expectedValue) {
     return this.if(
@@ -29,19 +32,27 @@ describe('finding files', function () {
     var g = slutDoc.getFiles;
 
     it('should return an object', function () {
-        will(g(path, 'js')).have(['path', 'extension', 'files']);
+        will(g(searchPath, 'js')).have(['path', 'extension', 'files']);
     });
 
     it('should use cwd if no path is provided', function () {
-        will(g(null, 'js').path).be(path);
+        will(g(null, 'js').path).be(process.cwd());
     });
 
     it('should use \'js\' if no extension is provided', function () {
-        will(g(path).extension).be('js');
+        will(g(searchPath).extension).be('js');
     });
 
-    // it('should find files by extension', function () {
-    //     slutDoc.getFiles(path, 'js');
-    //     will(slutDoc.matchedFileCount).beGreaterThan(0);
-    // });
+    it('should find only files with extension', function () {
+        var result = g(searchPath);
+        will(result.files.length).beGreaterThan(0);
+    });
+});
+
+describe.only('finding doc blocks', function () {
+    var foo = 'blah blah blah \\n/**\\n\\t* @param {Boolean} hey\\n\\t*/\\nmore';
+
+    it('should be able to find blocks', function () {
+        will(slutDoc.findBlock(foo)).be(true);
+    });
 });
