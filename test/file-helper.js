35c0bc8d4924c3bdc53617c807f92f85838840d0
Jeremy Greer <jeremygreer@reergymerej.com>
2014-06-14 22:55:25 -0400

    add test to ensure files are created

--- test/file-helper.js
+++ test/file-helper.js
@@ -3,6 +3,7 @@
 // TODO: Break this out into its own module.
 
 var path = require('path'),
+    fs = require('fs'),
     mocha = require('mocha'),
     willy = require('willy'),
     fileHelper = require('../file-helper');
@@ -107,10 +108,22 @@ describe('finding pattern in files', function () {
 });
 
 describe.only('reading files with buffers', function () {
-    var filePath = path.join(__dirname, 'dummy-source/read.txt');
 
     it('should get the contents of a file', function () {
+        var filePath = path.join(__dirname, 'dummy-source/read.txt');
         var result = fileHelper.readFileSync(filePath);
         will(result).be('This is cool.');
+    });
+
+    it('should create the file if it does not exist', function () {
+        var filePath = path.join(__dirname, 'dummy-source/ghost.txt');
+        var result = fileHelper.readFileSync(filePath);
+        var exists = fs.existsSync(filePath);
+
+        if (exists) {
+            fs.unlink(filePath);
+        }
+
+        will(exists).be(true);
     });
 });
