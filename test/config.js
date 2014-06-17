0cb41f65fe820eca9f4b5e33bd0b3156853c8d76
Jeremy Greer <jeremygreer@reergymerej.com>
2014-06-16 21:54:18 -0400

    break out configuration into a separate module

--- test/config.js
+++ test/config.js
@@ -5,74 +5,97 @@
 var mocha = require('mocha'),
     willy = require('willy'),
     will = willy.will,
-    whatsupdoc = require('../whatsupdoc'),
     fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    config = require('../config');
 
-describe('getting/setting config options', function () {
+// describe('getting/setting config options', function () {
 
-    beforeEach(function () {
-        whatsupdoc.configure({
-            a: 1,
-            b: 2,
-            c: 3
-        });
+//     beforeEach(function () {
+//         whatsupdoc.configure({
+//             a: 1,
+//             b: 2,
+//             c: 3
+//         });
+//     });
+
+//     it('should work when getting one value', function () {
+//         var config = whatsupdoc.configure('b');
+//         will(config).be(2);
+//     });
+
+//     it('should work when getting all values', function () {
+//         var config = whatsupdoc.configure();
+//         will(config).have(['a', 'b', 'c']);
+//     });
+
+//     it('should work when setting one value', function () {
+//         whatsupdoc.configure('b', 'potato');
+//         will(whatsupdoc.configure('b')).be('potato');
+//     });
+
+//     it('should work when setting multiple values', function () {
+//         whatsupdoc.configure({
+//             b: 3,
+//             c: 4,
+//             d: 5
+//         });
+
+//         will(whatsupdoc.configure()).have('abcd'.split(''));
+//     });
+// });
+
+// describe('initializing config', function () {
+//     var dir = path.join(__dirname, 'dummy-source');
+
+//     // beforeEach(function () {
+//     //     var oldName = path.join(dir, '.whatsupdocrc');
+//     //     var newName = path.join(dir, '.whatsupdocrc_');
+//     //     fs.renameSync(oldName, newName);
+//     // });
+
+//     // afterEach(function () {
+//     //     var oldName = path.join(dir, '.whatsupdocrc_');
+//     //     var newName = path.join(dir, '.whatsupdocrc');
+//     //     fs.renameSync(oldName, newName);
+//     // });
+
+//     it('should start with default values', function () {
+//         var settings;
+
+//         whatsupdoc.init(dir);
+//         settings = whatsupdoc.configure();
+
+//         will(settings).have(['extensions', 'include', 'exclude', 'outputFile']);
+
+//     });
+
+//     it('should set values based on .whatsupdocrc', function () {
+//         // create config file
+//         // var filePath = path.join()
+//         console.log(process.cwd());
+//     });
+// });
+
+describe.only('getting the configuration', function () {
+    it('should return an object', function () {
+        var c = config.getConfig(path.join(__dirname, 'config'));
+        will(c).beAn(Object);
     });
 
-    it('should work when getting one value', function () {
-        var config = whatsupdoc.configure('b');
-        will(config).be(2);
+    it('should return options specified in .whatsupdocrc if found', function () {
+        var c = config.getConfig(path.join(__dirname, 'config'));
+        will(c.custom).be(true);
     });
 
-    it('should work when getting all values', function () {
-        var config = whatsupdoc.configure();
-        will(config).have(['a', 'b', 'c']);
+    it('should return the default config if no .whatsupdocrc is found', function () {
+        var c = config.getConfig(path.join(__dirname, 'config-none'));
+        will(c.default).be(true);
     });
 
-    it('should work when setting one value', function () {
-        whatsupdoc.configure('b', 'potato');
-        will(whatsupdoc.configure('b')).be('potato');
-    });
-
-    it('should work when setting multiple values', function () {
-        whatsupdoc.configure({
-            b: 3,
-            c: 4,
-            d: 5
-        });
-
-        will(whatsupdoc.configure()).have('abcd'.split(''));
-    });
-});
-
-describe('initializing config', function () {
-    var dir = path.join(__dirname, 'dummy-source');
-
-    // beforeEach(function () {
-    //     var oldName = path.join(dir, '.whatsupdocrc');
-    //     var newName = path.join(dir, '.whatsupdocrc_');
-    //     fs.renameSync(oldName, newName);
-    // });
-
-    // afterEach(function () {
-    //     var oldName = path.join(dir, '.whatsupdocrc_');
-    //     var newName = path.join(dir, '.whatsupdocrc');
-    //     fs.renameSync(oldName, newName);
-    // });
-
-    it('should start with default values', function () {
-        var settings;
-
-        whatsupdoc.init(dir);
-        settings = whatsupdoc.configure();
-
-        will(settings).have(['extensions', 'include', 'exclude', 'outputFile']);
-
-    });
-
-    it('should set values based on .whatsupdocrc', function () {
-        // create config file
-        // var filePath = path.join()
-        console.log(process.cwd());
+    it('should throw an error if .whatsupdocrc is bad JSON', function () {
+        will(function () {
+            config.getConfig(path.join(__dirname, 'config-bad'));
+        }).throw();
     });
 });
